a299fa02b878a1b1f067901532b659fc163f6500
Nicolas Carreno <nicox2600@gmail.com>
2022-07-07 11:57:34 -0500

    Usuario-actualizar pro

--- src/consultas/usuarios_sql.ts
+++ src/consultas/usuarios_sql.ts
@@ -2,6 +2,10 @@ export const SQL_USUARIOS = {
   OBTENER_USUARIOS:'SELECT u.usuario_id, u.usuario_nombres, u.usuario_apellidos, u.usuario_documento, \
   u.usuario_telefono, r.rol_id, r.rol_nombre\
   FROM usuario u INNER JOIN roles r ON u.usuario_rol = r.rol_id',
+  OBTENER_USUARIO_UNICO:'SELECT u.usuario_id, u.usuario_nombres, u.usuario_apellidos, u.usuario_documento, \
+  u.usuario_telefono, r.rol_id, r.rol_nombre\
+  FROM usuario u INNER JOIN roles r ON u.usuario_rol = r.rol_id\
+  WHERE u.usuario_id = $1',
   ACTUALIZAR_USUARIO:'UPDATE usuarios \
   SET usuario_rol = $1\
   WHERE usuario_id=$2',
