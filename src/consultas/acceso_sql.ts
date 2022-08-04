d3bef09dd030e880645b0ec95ae3595358f72ad8
Nicolas Carreno <nicox2600@gmail.com>
2022-08-04 17:50:06 -0500

    Upload User image

--- src/consultas/acceso_sql.ts
+++ src/consultas/acceso_sql.ts
@@ -1,6 +1,6 @@
 export const SQL_ACCESO = {
   INICIAR: "SELECT u.usuario_id, ro.rol_id, ro.rol_nombre, u.usuario_nombres, \
-  (SELECT img_nombreprivado FROM imagenes i WHERE i.img_usuario_id = u.usuario_id)\
+  (SELECT img_nombre_privado FROM imagenes i WHERE i.img_usuario_id = u.usuario_id)\
   FROM usuario u INNER JOIN roles ro ON u.usuario_rol = ro.rol_id \
   INNER JOIN acceso acc ON u.usuario_id = acc.acceso_usuario_id \
   WHERE acc.acceso_correo=$1 AND acc.acceso_clave=$2",
