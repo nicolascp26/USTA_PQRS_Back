09c4a116484876f8ecaa34707e5faedbdc65a805
Nicolas Carreno <nicox2600@gmail.com>
2022-08-22 11:45:12 -0500

    Notificaciones

--- src/consultas/usuarios_sql.ts
+++ src/consultas/usuarios_sql.ts
@@ -25,15 +25,17 @@ export const SQL_USUARIOS = {
   DELETE FROM usuario WHERE usuario_id = $1 RETURNING usuario_id;',
   STATS_ADMIN: 'SELECT DISTINCT \
 (SELECT count(usuario_id) FROM usuario) as activos,\
-(SELECT count(mensaje_id) FROM mensaje WHERE mensaje_codpadre IS NULL AND mensaje_estado=1) as nuevos,\
-(SELECT count(mensaje_id) FROM mensaje WHERE mensaje_codpadre IS NULL AND mensaje_estado=2 OR mensaje_estado=3) as progreso,\
-(SELECT count(mensaje_id) FROM mensaje WHERE mensaje_codpadre IS NULL AND mensaje_estado=4) as terminados,\
+(SELECT count(mensaje_id) FROM mensaje WHERE mensaje_codpadre IS NULL AND mensaje_estado=1) as nuevas,\
+(SELECT count(mensaje_id) FROM mensaje WHERE mensaje_codpadre IS NULL AND mensaje_estado=2) as respondidas,\
+(SELECT count(mensaje_id) FROM mensaje WHERE mensaje_codpadre IS NULL AND mensaje_estado=3) as esperando,\
+(SELECT count(mensaje_id) FROM mensaje WHERE mensaje_codpadre IS NULL AND mensaje_estado=4) as terminadas,\
 (SELECT count(mensaje_id) FROM mensaje WHERE mensaje_codpadre IS NULL) as totales \
 FROM mensaje',
 STATS_USER: 'SELECT DISTINCT \
-(SELECT count(mensaje_id) FROM mensaje WHERE mensaje_codpadre IS NULL AND mensaje_estado=1 AND mensaje_id_usuario = $1) as nuevos,\
-(SELECT count(mensaje_id) FROM mensaje WHERE mensaje_codpadre IS NULL AND (mensaje_estado=2 OR mensaje_estado=3) AND mensaje_id_usuario = $1) as progreso,\
-(SELECT count(mensaje_id) FROM mensaje WHERE mensaje_codpadre IS NULL AND mensaje_estado=4 AND mensaje_id_usuario = $1) as terminados,\
+(SELECT count(mensaje_id) FROM mensaje WHERE mensaje_codpadre IS NULL AND mensaje_estado=1 AND mensaje_id_usuario = $1) as nuevas,\
+(SELECT count(mensaje_id) FROM mensaje WHERE mensaje_codpadre IS NULL AND mensaje_estado=2 AND mensaje_id_usuario = $1) as esperando,\
+(SELECT count(mensaje_id) FROM mensaje WHERE mensaje_codpadre IS NULL AND mensaje_estado=3 AND mensaje_id_usuario = $1) as respondidas,\
+(SELECT count(mensaje_id) FROM mensaje WHERE mensaje_codpadre IS NULL AND mensaje_estado=4 AND mensaje_id_usuario = $1) as terminadas,\
 (SELECT count(mensaje_id_usuario) FROM mensaje WHERE mensaje_codpadre IS NULL AND mensaje_id_usuario = $1) as totales \
 FROM mensaje',
 }
